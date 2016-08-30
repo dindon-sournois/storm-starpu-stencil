5bf638528e9d34a22b9e430e60c1fb60901cb833
Loris Lucido <loris.lucido@etu.u-bordeaux.fr>
2016-08-30 16:18:02 +0200

    update

--- starpu-1.2-stencils/locality_cache_oblivious.gp
+++ starpu-1.2-stencils/locality_cache_oblivious.gp
@@ -5,16 +5,16 @@ set terminal pdf
 set key outside top center
 set title  font ",15" norotate
 set xlabel "Problem Size (MB)"
-set ylabel "Completion time / Problem Size"
+set ylabel "Completion time / Problem Size (MB)"
 
-set output 'cache_oblivious_gpu1_limit300.pdf'
-set title "Limited memory 300 MB"
+set output 'cache_oblivious_gpu1_limit500.pdf'
+set title "Limited memory 500 MB"
 
 plot \
-     "./output/cache_oblivious_1_300.txt" \
+     "./output/cache_oblivious_1_500.txt" \
      using ($1):($2)/($1) \
      with linespoints title "cache-oblivious 1 GPU" ls 1 lw 2 pt 1, \
-     "./output/cache_oblivious_1_dmdar_300_1.txt" \
+     "./output/cache_oblivious_1_dmdar_500_1.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 1 GPU" ls 2 lw 2 pt 2, \
 
@@ -30,34 +30,34 @@ plot \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 1 GPU" ls 2 lw 2 pt 2, \
 
-set output 'cache_oblivious_gpu2_limit300.pdf'
-set title "Limited memory 300 MB"
+set output 'cache_oblivious_gpu2_limit500.pdf'
+set title "Limited memory 500 MB"
 
 plot \
-     "./output/cache_oblivious_2_300.txt" \
+     "./output/cache_oblivious_2_500.txt" \
      using ($1):($2)/($1)\
      with linespoints title "cache-oblivious 2 GPU" ls 1 lw 2 pt 1, \
-     "./output/cache_oblivious_2_dmdar_300_1.txt" \
+     "./output/cache_oblivious_2_dmdar_500_1.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=1" ls 2 lw 2 pt 2, \
-     "./output/cache_oblivious_2_dmdar_300_1.2.txt" \
+     "./output/cache_oblivious_2_dmdar_500_1.2.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=1.2" ls 3 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_300_1.4.txt" \
+     "./output/cache_oblivious_2_dmdar_500_1.4.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=1.4" ls 4 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_300_10.txt" \
-     using ($1):($2)/($1) \
-     with linespoints title "dmdar 2 GPU beta=10" ls 5 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_300_0.8.txt" \
+     "./output/cache_oblivious_2_dmdar_500_0.8.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=0.8" ls 6 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_300_0.6.txt" \
+     "./output/cache_oblivious_2_dmdar_500_0.6.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=0.6" ls 7 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_300_0.1.txt" \
-     using ($1):($2)/($1) \
-     with linespoints title "dmdar 2 GPU beta=0.1" ls 8 lw 2 pt -1, \
+     # "./output/cache_oblivious_2_dmdar_500_0.1.txt" \
+     # using ($1):($2)/($1) \
+     # with linespoints title "dmdar 2 GPU beta=0.1" ls 8 lw 2 pt -1, \
+     # "./output/cache_oblivious_2_dmdar_500_10.txt" \
+     # using ($1):($2)/($1) \
+     # with linespoints title "dmdar 2 GPU beta=10" ls 5 lw 2 pt -1, \
 
 
 set output 'cache_oblivious_gpu2_limit0.pdf'
@@ -76,15 +76,15 @@ plot \
      "./output/cache_oblivious_2_dmdar_0_1.4.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=1.4" ls 4 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_0_10.txt" \
-     using ($1):($2)/($1) \
-     with linespoints title "dmdar 2 GPU beta=10" ls 5 lw 2 pt -1, \
      "./output/cache_oblivious_2_dmdar_0_0.8.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=0.8" ls 6 lw 2 pt -1, \
      "./output/cache_oblivious_2_dmdar_0_0.6.txt" \
      using ($1):($2)/($1) \
      with linespoints title "dmdar 2 GPU beta=0.6" ls 7 lw 2 pt -1, \
-     "./output/cache_oblivious_2_dmdar_0_0.1.txt" \
-     using ($1):($2)/($1) \
-     with linespoints title "dmdar 2 GPU beta=0.1" ls 8 lw 2 pt -1, \
+     # "./output/cache_oblivious_2_dmdar_0_0.1.txt" \
+     # using ($1):($2)/($1) \
+     # with linespoints title "dmdar 2 GPU beta=0.1" ls 8 lw 2 pt -1, \
+     # "./output/cache_oblivious_2_dmdar_0_10.txt" \
+     # using ($1):($2)/($1) \
+     # with linespoints title "dmdar 2 GPU beta=10" ls 5 lw 2 pt -1, \
--- starpu-1.2-stencils/locality_test.sh
+++ starpu-1.2-stencils/locality_test.sh
@@ -2,42 +2,24 @@
 
 results=~/rev/internship/storm/starpu-1.2-stencils/where_to_check.txt
 echo CREATED DIRS : > "$results"
+
 host="attila"
 ratio=0.8
+vector=3
 
-if [ "$1" == "make" ]
-then
-    compile="make -j4"
-else
-    compile=""
-fi
+limit_mem_list="0 100"
+nbgpus_list="1 2"
+iter="200"
+limit_mem_fxt_list="0 100"
+domain_fxt_list="400"
+nbgpus_fxt_list="1 2"
 
-# logs=~/rev/internship/storm/starpu-1.2-stencils/locality_logs.txt
-# rm $logs
-
-# function locality() {
-#     d="build-simgrid$1"
-#     cd "$d" || exit
-#     echo "$d"
-#     $compile > /dev/null
-#     sched="`cat sched.txt`"
-#     for i in $sched
-#     do
-#         echo "$i"
-#         STARPU_SCHED=$i ./tests/datawizard/locality --domain-size 100 > /dev/null
-#         ./tools/starpu_fxt_tool -r "$ratio" -i /tmp/prof_file_llucido_0 > /dev/null
-#         mv locality.xpm ../output/locality_"$host"_"$i$1".xpm
-#         mv paje.trace ../output/paje_"$host"_"$i$1".trace
-#     done
-#     cd - > /dev/null
-# }
-
-# function test_all_xpm() {
-#     locality ""
-#     locality "_LOCALITY"
-#     locality "_LOCALITY_TASKS"
-#     locality "_BOTH"
-# }
+limit_mem_list="0 500"
+nbgpus_list="1 2"
+iter="100"
+limit_mem_fxt_list="0 500"
+domain_fxt_list="100"
+nbgpus_fxt_list="1 2"
 
 # function test_limit_mem() {
 #     $compile > /dev/null
@@ -99,18 +81,17 @@ fi
 function test_cache_oblivious() {
     $compile > /dev/null
     cd "build-simgrid-no-fxt" || exit
-    iter=200
-    vector=3
 
-    # for limit_mem in 0 300; do
-    for limit_mem in 0 ; do
-        for nbgpus in 1 2; do
-            if [[ "$nbgpus" -eq 1 ]]; then
-                betas="1"
-            else
-                betas="0.1 0.6 0.8 1 1.2 1.4 10"
-            fi
-            for sched in dmdar; do
+    for limit_mem in $limit_mem_list ; do
+        for nbgpus in $nbgpus_list ; do
+            for sched in dmdar modular-heft dmda lws; do
+                if [[ "$nbgpus" -eq 1 ]]; then
+                    betas="1"
+                elif [[ "$sched" != "dmdar" ]]; then
+                    betas="1"
+                else
+                    betas="0.1 0.6 0.8 1 1.2 1.4 10"
+                fi
                 for beta in $betas; do
                     filename=../output/cache_oblivious_"$nbgpus"_"$sched"_"$limit_mem"_"$beta".txt
                     echo "# PROBLEM_SIZE (MB) COMPLETION_TIME (ms)" > $filename
@@ -120,28 +101,31 @@ function test_cache_oblivious() {
             echo "# PROBLEM_SIZE (MB) COMPLETION_TIME (ms)" > $filename
         done
 
-        for (( d=20 ; d<=1000; d+=48 )); do
-            problem_size=$(expr $d \* $vector)
-            for nbgpus in 1 2; do
-                for sched in dmdar; do
+        # for (( d=20 ; d<=1000; d+=48 )); do
+        for (( d=20 ; d<=1000; d+=500 )); do
+            problem=$(expr "$d" \* "$vector")
+            for nbgpus in $nbgpus_list; do
+                for sched in dmdar modular-heft dmda lws; do
                     export STARPU_SCHED=$sched
                     if [[ "$nbgpus" -eq 1 ]]; then
+                        betas="1"
+                    elif [[ "$sched" != "dmdar" ]]; then
                         betas="1"
                     else
                         betas="0.1 0.6 0.8 1 1.2 1.4 10"
                     fi
                     for beta in $betas; do
-                        echo ; echo "starting $sched domain=$d beta=$beta"
+                        echo ; echo "starting $sched problem=$problem beta=$beta"
                         filename=../output/cache_oblivious_"$nbgpus"_"$sched"_"$limit_mem"_"$beta".txt
                         completed_time=$(STARPU_HOSTNAME=$host STARPU_NCPUS=0 STARPU_NCUDA=$nbgpus STARPU_NOPENCL=0 STARPU_LIMIT_CUDA_MEM=$limit_mem STARPU_SCHED_BETA=$beta ./tests/datawizard/locality --domain-size $d --vector-size $vector --iterations $iter --silent | grep "completion time" | cut -d \  -f4)
-                        echo "$problem_size $completed_time" >> $filename
+                        echo "$problem $completed_time" >> $filename
                     done
                 done
 
-                echo ; echo "starting cache oblivious $nbgpus $d"
+                echo ; echo "starting cache oblivious $nbgpus $problem"
                 completed_time=$(STARPU_HOSTNAME=$host STARPU_LIMIT_CUDA_MEM=$limit_mem ./tests/datawizard/locality --domain-size $d --vector-size $vector --silent --iterations $iter --cache-oblivious --co-nbgpus $nbgpus | grep "completion time" | cut -d \  -f4)
                 filename=../output/cache_oblivious_"$nbgpus"_"$limit_mem".txt
-                echo "$problem_size $completed_time" >> $filename
+                echo "$problem $completed_time" >> $filename
             done
         done
     done
@@ -153,24 +137,22 @@ function test_cache_oblivious() {
 function test_cache_oblivious_details() {
     $compile > /dev/null
     cd "build-simgrid/output" || exit
-    vector=3
-    iter=200
 
-    for domain in 400; do
-        # for limit_mem in 0 100; do
-        for limit_mem in 0 ; do
-            foldername=d"$domain"_l"$limit_mem"_i"$iter"/
+    for domain in $domain_fxt_list; do
+        problem=$(expr "$d" \* "$vector")
+        for limit_mem in $limit_mem_fxt_list; do
+            foldername=d"$problem"_l"$limit_mem"_i"$iter"/
             mkdir $foldername 2> /dev/null
             cd $foldername && echo $foldername >> "$results"
             echo $foldername
 
-            for nbgpus in 1 2; do
-                for sched in dmdar; do
+            for nbgpus in $nbgpus_fxt_list; do
+                for sched in dmdar modular-heft; do
                     targetname="$sched"_gpu"$nbgpus"/
                     mkdir $targetname 2> /dev/null
                     cd $targetname && echo $targetname >> "$results"
-                    echo "starting $sched gpu $nbgpus domain $domain iter $iter limit $limit_mem"
-                    STARPU_HOSTNAME=$host STARPU_NCPUS=0 STARPU_NCUDA=$nbgpus STARPU_NOPENCL=0 STARPU_LIMIT_CUDA_MEM=$limit_mem STARPU_SCHED=$sched ../../../tests/datawizard/locality --domain-size $domain --vector-size $vector --iterations $iter --silent
+                    echo "starting $sched gpu $nbgpus problem $problem iter $iter limit $limit_mem"
+                    STARPU_HOSTNAME=$host STARPU_NCPUS=0 STARPU_NCUDA=$nbgpus STARPU_NOPENCL=0 STARPU_LIMIT_CUDA_MEM=$limit_mem STARPU_SCHED=$sched ../../../tests/datawizard/locality --domain-size $domain --vector-size $vector --iterations $iter --silent --cfg=maxmin/precision:0.0001
                     ../../../tools/starpu_fxt_tool -i /tmp/prof_file_llucido_0 -r "$ratio"
                     cd .. > /dev/null
                 done
@@ -178,8 +160,8 @@ function test_cache_oblivious_details() {
                 targetname=co_gpu"$nbgpus"/
                 mkdir $targetname 2> /dev/null
                 cd $targetname && echo $targetname >> "$results"
-                echo "starting cache oblivious gpu $nbgpus domain $domain iter $iter limit $limit_mem"
-                STARPU_HOSTNAME=$host STARPU_LIMIT_CUDA_MEM=$limit_mem ../../../tests/datawizard/locality --domain-size $domain --vector-size $vector --cache-oblivious --iterations $iter --co-nbgpus $nbgpus --silent
+                echo "starting cache oblivious gpu $nbgpus problem $problem iter $iter limit $limit_mem"
+                STARPU_HOSTNAME=$host STARPU_LIMIT_CUDA_MEM=$limit_mem ../../../tests/datawizard/locality --domain-size $domain --vector-size $vector --cache-oblivious --iterations $iter --co-nbgpus $nbgpus --silent --cfg=maxmin/precision:0.0001
                 ../../../tools/starpu_fxt_tool -i /tmp/prof_file_llucido_0 -r "$ratio"
                 cd .. > /dev/null
             done
@@ -194,17 +176,15 @@ function test_cache_oblivious_details() {
 function test_prefetch() {
     $compile > /dev/null
     cd "build-simgrid/output" || exit
-    vector=3
-    iter=200
 
-    for domain in 400; do
-        # for limit_mem in 0 100; do
-        for limit_mem in 0 ; do
-            foldername=d"$domain"_l"$limit_mem"_i"$iter"/
+    for domain in $domain_fxt_list; do
+        problem=$(expr "$d" \* "$vector")
+        for limit_mem in $limit_mem_fxt_list; do
+            foldername=d"$problem"_l"$limit_mem"_i"$iter"/
             mkdir $foldername 2> /dev/null
             cd $foldername && echo $foldername >> "$results"
             for opt in regular prefetch-data; do
-                echo ; echo "starting dmdar gpu 2 domain $domain iter $iter limit $limit_mem $opt"
+                echo ; echo "starting dmdar gpu 2 problem $problem iter $iter limit $limit_mem $opt"
                 targetname=dmdar_gpu2_"$opt"/
                 mkdir $targetname 2> /dev/null
                 cd $targetname && echo $targetname >> "$results"
@@ -223,18 +203,16 @@ function test_prefetch() {
 function test_prio() {
     $compile > /dev/null
     cd "build-simgrid/output" || exit
-    vector=3
-    iter=200
 
-    for domain in 400; do
-        # for limit_mem in 0 100; do
-        for limit_mem in 0 ; do
-            foldername=d"$domain"_l"$limit_mem"_i"$iter"/
+    for domain in $domain_fxt_list; do
+        problem=$(expr "$d" \* "$vector")
+        for limit_mem in $limit_mem_fxt_list; do
+            foldername=d"$problem"_l"$limit_mem"_i"$iter"/
             mkdir $foldername 2> /dev/null
             cd $foldername && echo $foldername >> "$results"
             for nbcpus in 1 2 4; do
                 for sched in prio eager; do
-                    echo ; echo "starting $sched cpu $nbcpus domain $domain iter $iter limit $limit_mem"
+                    echo ; echo "starting $sched cpu $nbcpus problem $problem iter $iter limit $limit_mem"
                     targetname="$sched"_cpu"$nbcpus"/
                     mkdir $targetname 2> /dev/null
                     cd $targetname && echo $targetname >> "$results"
@@ -250,10 +228,6 @@ function test_prio() {
     done
     cd ../.. > /dev/null
 }
-
-# test_all_xpm
-# test_limit_mem
-# test_increase_problem_size
 
 test_cache_oblivious && ./locality_cache_oblivious.gp
 test_cache_oblivious_details
